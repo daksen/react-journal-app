6dfa720ca8cf1dd6135647a2a4cfc963c6f87f1e
DakSen <carlos.tavares@alumnos.udg.mx>
2021-05-30 06:42:09 -0500

    firebase example

---
+++ src/firebase/firebaseConfigExample.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export {
+  db,
+  googleAuthProvider,
+  firebase
+}
